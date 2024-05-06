cd164cf68e0785c4b3e02e204d9641e42abfe918
SebastianMT1 <00161024@uca.edu.sv>
2024-05-06 12:37:53 -0600

    ejercicios menus, tablas

---
+++ while_menu.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    char letra;
+
+    while ( letra != 'x')
+    {
+        cout<<"\nopciones del menu de almuerzo:\n \n";
+        cout<<"Sopa de pollo\n";
+        cout<<"Milanesa\n";
+        cout<<"Pollo asado\n";
+        cout<<"pollo guisado\n";
+        cout<<"Carne asada\n";
+
+        cin>>letra;
+    }
+    
+}
